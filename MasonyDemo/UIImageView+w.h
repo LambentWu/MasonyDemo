49267af6be95cb7d930ab82535194dc5e56811f4
wudev <wudev@163.com>
2020-12-06 02:59:40 +0800

    qq

---
+++ MasonyDemo/UIImageView+w.h
@@ -0,0 +1,16 @@
+//
+//  UIImageView+w.h
+//  MasonyDemo
+//
+//  Created by Lambent on 2020/12/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImageView (w)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MasonyDemo/UIImageView+w.m
@@ -0,0 +1,12 @@
+//
+//  UIImageView+w.m
+//  MasonyDemo
+//
+//  Created by Lambent on 2020/12/6.
+//
+
+#import "UIImageView+w.h"
+
+@implementation UIImageView (w)
+
+@end
